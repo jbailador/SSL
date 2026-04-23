06e15c049377531684fc6d2ba1e303d8810f14a7
Santino Lauria <slauria@frba.utn.edu.ar>
2026-04-23 19:39:37 -0300

    optimizamos las funciones del punto 2 exepto power y toUpper

--- DD-Strings/String.c
+++ DD-Strings/String.c
@@ -3,69 +3,60 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int getLength(char* cadena){
-    int cont = 0;
-    for (int i = 0; cadena[i] != '\0' ; i++)
-    {
-       cont++;        
+int getLength(const char* cadena) {
+    if (*cadena == '\0') {
+        return 0;
     }
-    return cont;
+    return 1 + getLength(cadena + 1);
 }
 
 bool isEmpty(const char *cadena) {
-    // Solo miramos si el primer carácter (cadena[0]) es el fin de cadena ('\0')
     return cadena[0] == '\0'; 
 }
 
-bool areDecimalDigits(char* cadena) {
-    int contInt = 0;
-    int longitud = getLength(cadena);
-    bool resultado = false;
-    for(int i = 0; i < longitud; i++) {
-        if(cadena[i] >= 48 && cadena[i] <= 57) {
-            contInt++;
+bool areDecimalDigits(const char* cadena) {
+    if (cadena[0] == '\0') {
+        return false;
+    }
+    for (int i = 0; cadena[i] != '\0'; i++) {
+        if (cadena[i] < '0' || cadena[i] > '9') {
+            return false;
         }
     }
-    if(contInt == longitud) {
-        resultado = true;
+    return true; 
+}
+
+int toInteger(char* cadena) {
+    int resultado = 0;
+    for (int i = 0; cadena [i] != '\0'; i++)
+    {
+        resultado = (resultado * 10) + (cadena[i] - '0');
     }
     return resultado;
-}
-    
-int toInteger(char* cadena) {
-    
-    return atoi(cadena);
 }
 
 bool contains(char* cadena, char caracter) {
 
-    for(int i = 0; i < getLength(cadena); i++) {
+    for(int i = 0; i != '\0'; i++) {
         if(cadena[i] == caracter) {
             return true;
         }
     }
-
     return false;
 }
 
-bool areEqual(char* cadena1, char* cadena2){
-   
-    if(getLength(cadena1)!=getLength(cadena2)){
-        return false;
-    }else {
-         for (int i = 0; i < getLength(cadena1); i++)
-    {
-        if (cadena1[i]!=cadena2[i])
-        {
-            return false;
-        }
-        
+bool areEqual(const char* cadena1, const char* cadena2) {
+    if (*cadena1 == '\0' && *cadena2 == '\0') {
+        return true;
     }
+    
+    if (*cadena1 != *cadena2) {
+        return false;
+    }
+    
+    return areEqual(cadena1 + 1, cadena2 + 1);
 }
     
-return true;
-}
-
 char* concatenate(char* cadena1, char* cadena2){
     int total = getLength(cadena1)+getLength(cadena2);
     char* cadenaConcatenada = malloc(total+1);
--- DD-Strings/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char const *argv[])
-{
-    char* cadena = "Hola";
-    printf("%s", *cadena);
-    return 0;
-}

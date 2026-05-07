a27b10089bf2a0ff34bba160bdb87a67e1f2c905
Santino Lauria <slauria@frba.utn.edu.ar>
2026-05-06 23:50:17 -0300

    Se corrige de nuevo mayorlongitud.c utilizando punteros

---
+++ DD-Strings/mayorlongitud.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "String.h"
+
+int main(int argc, char argv[]) {
+
+    if (argc < 2) return 0;
+
+
+    char mayor = (argv + 1);
+    int maxLen = getLength(mayor);
+
+
+    for (char **p = argv + 2;p != NULL; p++) {
+
+        int actualLen = getLength(p); // Medimos la palabra actual
+
+        if (actualLen > maxLen) {
+            maxLen = actualLen; 
+            mayor =p;
+        }
+    }
+
+    printf("%s\n", mayor);
+
+    return 0;
+}

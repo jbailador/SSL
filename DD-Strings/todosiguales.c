f97a7703fcd655c0fd485eb57ca36c594b5b4bd6
Santino Lauria <slauria@frba.utn.edu.ar>
2026-05-06 23:36:03 -0300

    Se corrige de nuevo todosiguales.c utilizando punteros

--- DD-Strings/todosiguales.c
+++ DD-Strings/todosiguales.c
@@ -9,7 +9,7 @@ int main(int argc, char argv[]) {
     for (char **p = argv + 1;p != NULL && (p + 1) != NULL; p++) {
 
         if (!AreEqual(p, *(p + 1))) {
-            igual/es = 0; / ¡Encontró una diferencia! Anota un 0 y deja de buscar.
+            iguales = 0;
             break;
         }
     }

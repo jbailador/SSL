60a2ee34b2fa01c2da29273687c110d132f53af5
Santino Lauria <slauria@frba.utn.edu.ar>
2026-05-11 23:15:31 -0300

    GetLenght corregido x2

--- DD-Strings/longitudes.c
+++ DD-Strings/longitudes.c
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
     (void) argc;
     for (char **p = argv + 1;p != NULL; p++)
     {
-        printf ("%d\n", getLength(*p));
+        printf ("%d\n", GetLength(*p));
     }
     return 0;
 }
